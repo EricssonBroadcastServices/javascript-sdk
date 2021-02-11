1dfe8550e6cbd73716b003174d4f451d22a98dd2
Benjamin Wallberg <me@bwallberg.com>
2021-02-11 12:48:36 +0100

    fix: date-fns import not working universally

--- packages/whitelabel/src/utils/time.ts
+++ packages/whitelabel/src/utils/time.ts
@@ -1,5 +1,5 @@
 import { parse } from "iso8601-duration";
-import format from "date-fns/format";
+import { format } from "date-fns";
 import * as humanizeDuration from "humanize-duration";
 
 export interface Duration {
