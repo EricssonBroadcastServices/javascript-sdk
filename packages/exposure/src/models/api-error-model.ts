00c2e37a890c7e7481fb9a045d082aecc8e40b3f
simonwallin <simon.wallin@eyevinn.se>
2021-03-12 11:58:54 +0100

    fix: handle missing err object

--- packages/exposure/src/models/api-error-model.ts
+++ packages/exposure/src/models/api-error-model.ts
@@ -1,8 +1,8 @@
 export class ApiError {
   public message: string;
   public httpCode: number;
-  constructor(err) {
-    this.message = err.message;
-    this.httpCode = err.httpCode;
+  constructor(err: any) {
+    this.message = err?.message;
+    this.httpCode = err?.httpCode;
   }
 }
