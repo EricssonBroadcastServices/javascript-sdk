5ba725ba50080e75b923624d360dfdabfd38a597
Benjamin Wallberg <me@bwallberg.com>
2021-03-23 12:53:46 +0100

    feat(etcd-client): support getting full directories, improve watcher

--- packages/etcd-client/src/index.ts
+++ packages/etcd-client/src/index.ts
@@ -3,6 +3,20 @@ import * as dns from "dns";
 
 let etcd: Etcd;
 
+function convertNodeToJson<T>(node: any): T {
+  if (node.dir) {
+    const directory = {};
+    node.nodes.forEach((node: any) => {
+      const paths = node.key.split("/");
+      const path = paths[paths.length - 1];
+      directory[path] = convertNodeToJson(node);
+    });
+    return directory as T;
+  } else {
+    return node.value;
+  }
+}
+
 /**
  * Connect to ETCD using the provided SRV domain
  * @param  {string}        srv  the SRV domain without the etcd prefixes used by RBM
@@ -24,23 +38,44 @@ export function connect(srv: string): Promise<void> {
 }
 
 /**
+ * Get the provided key eg. /foo/bar
+ * @param {string}            key         path to the key in ETCD
+ * @return {Promise<string>} resolves with string if OK, rejects with any error
+ */
+export function get<T>(key: string): Promise<T> {
+  return new Promise((resolve, reject) => {
+    etcd.get(key, { recursive: true }, (err: any, success: any) => {
+      if (success) {
+        const { node } = success;
+        resolve(convertNodeToJson(node));
+      } else {
+        reject(new Error(`[node-etcd] ${err}`));
+      }
+    });
+  });
+}
+
+/**
  * Watch the provided key eg. /foo/bar and also get the initial value
  * @param {string}            key         path to the key in ETCD
  * @param {IEtcdClientError, string) =>  void}  callback    function to be called when the value is available/has changed
  */
-
-export function watch(key: string, callback: (error: Error | null, value: string | null) => void) {
-  etcd.watcher(key).on("change", ({ node }) => {
-    if (node) {
-      callback(null, node.value);
-    }
-  });
-  etcd.get(key, (err: any, success: any) => {
-    if (success) {
-      const { node } = success;
-      callback(null, node.value);
-    } else {
-      callback(new Error(`[node-etcd] ${err}`), null);
-    }
-  });
+export function watch<T>(key: string, interval = 60 * 1000, callback: (error: Error | null, value: T | null) => void): () => void {
+  let timeout;
+  const getWatchedKey = () => {
+    get<T>(key).then(
+      value => {
+        callback(null, value);
+        timeout = setTimeout(getWatchedKey, interval);
+      },
+      err => {
+        callback(err, null);
+        timeout = setTimeout(getWatchedKey, interval);
+      }
+    );
+  };
+  getWatchedKey();
+  return () => {
+    clearTimeout(timeout);
+  }
 }
