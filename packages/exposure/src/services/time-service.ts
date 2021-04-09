c34a5cb0e887ecce2bca841f66fcfc6dc9d96288
Erik Hoffman <erik.johansson87@gmail.com>
2021-04-09 15:13:22 +0200

    fix: get time aligned with server (#42)
    * storage layer to be compatible with both node and browser
    
    * store time diff between client and server and utilies for resolving times
    
    * fix: check if sessionStorage is supported

--- packages/exposure/src/services/time-service.ts
+++ packages/exposure/src/services/time-service.ts
@@ -1,8 +1,34 @@
-import { BaseService, CustomerAndBusinessUnitOptions } from "./base-service";
+import { BaseService, CustomerAndBusinessUnitOptions, ServiceOptions } from "./base-service";
 import { deserialize } from "../decorators/property-mapper";
 import { Time } from "../models/time-model";
+import { sessionKeyStorage } from "../utils/storage";
+
+interface GetNowOptions extends CustomerAndBusinessUnitOptions {
+  clientTime: number;
+}
+
+const SESSION_STORAGE_KEY = "clientTimeDiff";
 
 export class TimeService extends BaseService {
+  constructor(public options: ServiceOptions) {
+    super(options);
+
+    // if possible we can set up the diff between client and server already on initialization
+    this.getClientTimeDiff({
+      customer: options.customer,
+      businessUnit: options.businessUnit
+    });
+  }
+
+  private getClientTimeDiff({ customer, businessUnit }: CustomerAndBusinessUnitOptions) {
+    if (!customer || !businessUnit) return;
+    this.getNow({
+      customer,
+      businessUnit,
+      clientTime: Date.now()
+    });
+  }
+
   public getTime({ customer, businessUnit }: CustomerAndBusinessUnitOptions) {
     return this.get(
       `${this.cuBuUrl({
@@ -11,5 +37,22 @@ export class TimeService extends BaseService {
         apiVersion: "v1"
       })}/time`
     ).then(data => deserialize(Time, data));
+  }
+
+  public async getNow({ customer, businessUnit, clientTime }: GetNowOptions): Promise<number> {
+    // if a diff is stored, respond with client time adjusted accordingly
+    if (sessionKeyStorage.getItem(SESSION_STORAGE_KEY)) {
+      const diff = sessionKeyStorage.getItem(SESSION_STORAGE_KEY);
+      return Number(clientTime) + Number(diff);
+    } else {
+      // if we do not have a diff stored, fetch it and store it - though respond with the server time for now
+      const timeResponse = await this.getTime({ customer, businessUnit });
+      const epochTime = timeResponse.epochMillis;
+      if (!sessionKeyStorage.getItem(SESSION_STORAGE_KEY)) {
+        const diff = clientTime - epochTime;
+        sessionKeyStorage.setItem(SESSION_STORAGE_KEY, diff.toString());
+      }
+      return epochTime;
+    }
   }
 }
---
+++ packages/exposure/src/utils/storage.ts
@@ -0,0 +1,31 @@
+const _storage = {};
+
+const store = (key: string, value: string): void => {
+  if (typeof sessionStorage !== "undefined") {
+    sessionStorage.setItem(key, value);
+  } else {
+    _storage[key] = value;
+  }
+};
+
+const get = (key: string): string | null => {
+  if (typeof sessionStorage !== "undefined") {
+    return sessionStorage.getItem(key);
+  } else {
+    return _storage[key];
+  }
+};
+
+const remove = (key: string): void => {
+  if (typeof sessionStorage !== "undefined") {
+    sessionStorage.removeItem(key);
+  } else {
+    delete _storage[key];
+  }
+};
+
+export const sessionKeyStorage = {
+  setItem: (key, value) => store(key, value),
+  getItem: key => get(key),
+  remove: key => remove(key)
+};
