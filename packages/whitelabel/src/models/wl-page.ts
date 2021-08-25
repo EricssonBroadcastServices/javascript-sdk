7be63cf30a82baf6ba885a16cbe3b48f43aba398
Simon Wallin <simon.wallin@eyevinn.se>
2021-08-25 10:23:44 +0200

    feat: add IWLPage interface (#83)
    * feat: add IWLPage interface
    
    * fix: add page interface

--- packages/whitelabel/src/models/wl-page.ts
+++ packages/whitelabel/src/models/wl-page.ts
@@ -1,8 +1,15 @@
 import { WLComponent } from "./wl-component";
 import { WLReference } from "./wl-reference";
 import {jsonProperty} from "@ericssonbroadcastservices/exposure-sdk";
+import { IWLPage } from "../interfaces/wl-page";
 
-export class WLPageModel extends WLComponent {
+export class WLPageModel extends WLComponent implements IWLPage {
   @jsonProperty({ type: WLReference })
   public components: WLReference[]
+  @jsonProperty()
+  public name: string;
+  @jsonProperty()
+  public title?: string;
+  @jsonProperty()
+  public description?: string;
 }
