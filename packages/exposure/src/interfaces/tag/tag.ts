29a032f1cdc6636c3ad12675ce6b009074df5757
simonwallin <simon.wallin@eyevinn.se>
2021-10-01 12:11:24 +0200

    fix: expanded tag-model

--- packages/exposure/src/interfaces/tag/tag.ts
+++ packages/exposure/src/interfaces/tag/tag.ts
@@ -1,5 +1,15 @@
+import { ImageOrientation, ImageType } from "../..";
+
+interface ITagImage {
+  height: number;
+  orientation: ImageOrientation;
+  type: ImageType;
+  url: string;
+  width: number;
+}
+
 export interface ITag {
   tagId: string;
-  localized: { title?: string; locale: string }[];
+  localized: { title?: string; locale: string; description?: string; images: ITagImage[] }[];
   scheme: string;
 }
--- packages/exposure/src/utils/tag.ts
+++ packages/exposure/src/utils/tag.ts
@@ -8,6 +8,15 @@ function getTitle(tag: ITag, locale: string) {
   return localeItem.title || "";
 }
 
+function getDescription(tag: ITag, locale: string) {
+  if (tag.localized.length === 0) {
+    return "";
+  }
+  const localeItem = tag.localized.find(localizedItem => localizedItem.locale === locale) || tag.localized[0];
+  return localeItem.description || "";
+}
+
 export const tagUtils = {
-  getTitle
+  getTitle,
+  getDescription
 };
