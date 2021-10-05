1e1c9b6cfceab33e322c795d86718959cd6f9e12
simonwallin <simon.wallin@eyevinn.se>
2021-10-05 12:26:44 +0200

    feat: add optional default locale to tagUtils

--- packages/exposure/src/utils/tag.ts
+++ packages/exposure/src/utils/tag.ts
@@ -1,18 +1,24 @@
 import { ITag } from "../interfaces/tag/tag";
 
-function getTitle(tag: ITag, locale: string) {
+function getTitle(tag: ITag, locale: string, defaultLocale?: string) {
   if (tag.localized.length === 0) {
     return "";
   }
-  const localeItem = tag.localized.find(localizedItem => localizedItem.locale === locale) || tag.localized[0];
+  const localeItem =
+    tag.localized.find(localizedItem => localizedItem.locale === locale) ||
+    tag.localized.find(localizedItem => localizedItem.locale === defaultLocale) ||
+    tag.localized[0];
   return localeItem.title || "";
 }
 
-function getDescription(tag: ITag, locale: string) {
+function getDescription(tag: ITag, locale: string, defaultLocale?: string) {
   if (tag.localized.length === 0) {
     return "";
   }
-  const localeItem = tag.localized.find(localizedItem => localizedItem.locale === locale) || tag.localized[0];
+  const localeItem =
+    tag.localized.find(localizedItem => localizedItem.locale === locale) ||
+    tag.localized.find(localizedItem => localizedItem.locale === defaultLocale) ||
+    tag.localized[0];
   return localeItem.description || "";
 }
 
