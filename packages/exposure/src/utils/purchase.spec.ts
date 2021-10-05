8797c2224d3125c5c8180b1259119de5f3cfb2e4
simonwallin <simon.wallin@eyevinn.se>
2021-10-05 13:19:49 +0200

    test: add simple purchase test

---
+++ packages/exposure/src/utils/purchase.spec.ts
@@ -0,0 +1,30 @@
+import { IPurchase, PurchaseStatus, purchaseUtils } from "..";
+
+describe("purchaseUtils", () => {
+  it("should return all tvods", () => {
+    const purchases: IPurchase[] = [
+      {
+        id: "123",
+        productOfferingId: "123",
+        transactionId: "123",
+        from: new Date().toISOString(),
+        until: new Date(Date.now() + 10000).toISOString(),
+        status: PurchaseStatus.FULFILLED,
+        renewal: false
+      },
+      {
+        id: "456",
+        productOfferingId: "123",
+        transactionId: "123",
+        from: new Date().toISOString(),
+        until: new Date(Date.now() + 10000).toISOString(),
+        status: PurchaseStatus.FULFILLED,
+        renewal: false,
+        assetId: "123"
+      }
+    ];
+    const tvods = purchaseUtils.getTvods(purchases);
+    expect(tvods.length).toBe(1);
+    expect(tvods[0].id).toBe("456");
+  });
+});
