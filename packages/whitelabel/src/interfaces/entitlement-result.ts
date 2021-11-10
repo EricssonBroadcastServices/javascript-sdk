2ade7a2f7c4a1d32acb2696cc2e66f6f00f1b03d
Simon Wallin <simon.wallin@eyevinn.se>
2021-11-10 12:33:36 +0100

    fix: should wait check on entitlement util (#113)

--- packages/whitelabel/src/interfaces/entitlement-result.ts
+++ packages/whitelabel/src/interfaces/entitlement-result.ts
@@ -13,4 +13,6 @@ export interface IEntitlementStatusResult {
   // return a date if all the user has to do is wait
   startTime: null | Date;
   loginToWatchForFree: boolean;
+  // if the user should just wait. This will be true if a wait actions is before or at the same time as any accessLater offering
+  shouldJustWait: boolean;
 }
