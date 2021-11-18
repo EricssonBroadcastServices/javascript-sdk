faf34722c40d30228927855c3a4c3f086e451571
simonwallin <simon.wallin@eyevinn.se>
2021-11-18 11:22:32 +0100

    fix: add missing export

--- packages/whitelabel-react/src/index.ts
+++ packages/whitelabel-react/src/index.ts
@@ -1,6 +1,6 @@
 export { RedBeeContext, RedBeeProvider, ActionType, useRedBeeState, useRedBeeStateDispatch } from "./RedBeeProvider";
 export type { IStorage, IDevice } from "./RedBeeProvider";
-export { useSelectedLanguage } from "./hooks/useSelectedLanguage";
+export { useSelectedLanguage, useSetSelectedLanguage } from "./hooks/useSelectedLanguage";
 export { useExposureApi, useWLApi } from "./hooks/useApi";
 export { useGeolocation } from "./hooks/useGeolocation";
 export { useConfig, useFetchConfig, useTheme } from "./hooks/useConfig";
