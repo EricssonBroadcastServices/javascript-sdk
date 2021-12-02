ef3ad6559c1be8aeaacb2a55fa6e33b483285054
Erik <erik.johansson87@gmail.com>
2021-12-02 09:51:46 +0100

    fixup! feat: ad clickthrough url and tracking (#119)

--- packages/exposure/src/models/play-model.ts
+++ packages/exposure/src/models/play-model.ts
@@ -140,7 +140,7 @@ export class AdClickThroughData {
   @jsonProperty()
   public clickThroughUrl: string;
   @jsonProperty()
-  public clickTrackingUrls: string;
+  public clickTrackingUrls: string[];
 }
 
 export class AdClip {
