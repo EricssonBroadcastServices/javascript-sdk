e696e8ec285a36eb8012b672947907522b200d1a
Simon Wallin <simon.wallin@eyevinn.se>
2022-04-19 14:31:26 +0200

    fix: add simple searchV3 method (#144)

--- packages/exposure/src/services/search-service.ts
+++ packages/exposure/src/services/search-service.ts
@@ -3,18 +3,26 @@ import { BaseService, CustomerAndBusinessUnitOptions } from "./base-service";
 import { deserialize } from "../decorators/property-mapper";
 import { Asset } from "../models/asset-model";
 import { IAssetResponse } from "../interfaces/content/asset-response";
+import { ISearchV3Response } from "../interfaces/search/search";
 
 export interface SearchOptions extends CustomerAndBusinessUnitOptions {
   query: string;
   locale: string;
+  types?: string[];
 }
 
 export class SearchService extends BaseService {
-  public search({ customer, businessUnit, query, locale }: SearchOptions): Promise<IAssetResponse> {
+  public search({
+    customer,
+    businessUnit,
+    query,
+    locale,
+    types = ["MOVIE", "TV_SHOW", "EPISODE", "CLIP", "TV_CHANNEL"]
+  }: SearchOptions): Promise<IAssetResponse> {
     const requestQuery = {
       fieldSet: "ALL",
       locale,
-      types: "MOVIE,TV_SHOW,EPISODE,CLIP,TV_CHANNEL"
+      types: types.join(",")
     };
     return this.get(
       `${this.cuBuUrl({
@@ -25,6 +33,33 @@ export class SearchService extends BaseService {
     ).then(data => ({
       ...data,
       items: data.items.map(i => deserialize(Asset, i.asset))
+    }));
+  }
+
+  public searchV3({
+    customer,
+    businessUnit,
+    locale,
+    query,
+    types = ["MOVIE", "TV_SHOW", "EPISODE", "CLIP", "TV_CHANNEL"]
+  }: SearchOptions): Promise<ISearchV3Response> {
+    const requestQuery = {
+      fieldSet: "ALL",
+      locale,
+      types: types.join(",")
+    };
+    return this.get(
+      `${this.cuBuUrl({
+        apiVersion: "v3",
+        customer,
+        businessUnit
+      })}/content/search/query/${query}?${querystring.stringify(requestQuery)}`
+    ).then(data => ({
+      ...data,
+      assetHits: {
+        ...data.assetHits,
+        items: data.assetHits.items.map(a => ({ asset: deserialize(Asset, a.asset) }))
+      }
     }));
   }
 }
