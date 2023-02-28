5849a859ed90fc21c4748126c6ed2858c8db0aa2
oscnord <oscar.nord@eyevinn.se>
2023-02-28 16:30:41 +0100

    chore(EMP-19290): Return location based on business unit and customer if they exists

--- packages/exposure/src/services/location-service.ts
+++ packages/exposure/src/services/location-service.ts
@@ -3,6 +3,9 @@ import { IUserLocation } from "../interfaces/location/user-location";
 
 export class LocationService extends BaseService {
   public getLocation(): Promise<IUserLocation> {
+    if (this.options.businessUnit && this.options.customer) {
+      return this.get(`/v1/customer/${this.options.customer}/businessunit/${this.options.businessUnit}/location`);
+    }
     return this.get("/v2/location");
   }
 }
