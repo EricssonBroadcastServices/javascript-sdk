cef5393cbc34366a65cfbfa9c539cf8fa4a4e345
oscnord <oscar.nord@eyevinn.se>
2023-03-01 09:51:11 +0100

    chore: use `cuBuUrl()`

--- packages/exposure/src/services/location-service.ts
+++ packages/exposure/src/services/location-service.ts
@@ -4,7 +4,7 @@ import { IUserLocation } from "../interfaces/location/user-location";
 export class LocationService extends BaseService {
   public getLocation(): Promise<IUserLocation> {
     if (this.options.businessUnit && this.options.customer) {
-      return this.get(`/v1/customer/${this.options.customer}/businessunit/${this.options.businessUnit}/location`);
+      return this.get(`${this.cuBuUrl({ apiVersion: "v1" })}/location`);
     }
     return this.get("/v2/location");
   }
