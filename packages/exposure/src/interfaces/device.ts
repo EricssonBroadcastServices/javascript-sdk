194e4754470b619a897e8583ed552caf311aa929
Benjamin Wallberg <benjamin.wallberg@eyevinn.se>
2023-03-22 06:52:08 +0100

    chore: add missing `DeviceType` values

--- packages/exposure/src/interfaces/device.ts
+++ packages/exposure/src/interfaces/device.ts
@@ -1,10 +1,18 @@
 export enum DeviceType {
   WEB = "WEB",
-  SMART_TV = "SMART_TV"
+  MOBILE = "MOBILE",
+  TABLET = "TABLET",
+  SMART_TV = "SMART_TV",
+  APPLE_TV = "APPLE_TV",
+  CONSOLE = "CONSOLE",
+  STB = "STB"
 }
 
 export interface IDeviceInfo {
   type: DeviceType;
+  /**
+   * The user's name of the device.
+   */
   name: string;
   deviceId: string;
 }
