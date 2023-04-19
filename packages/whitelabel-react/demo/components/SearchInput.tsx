d6d4ad9c0c1fad03e7598b4a3d4bbe49d3c883d2
Simon Wallin <simon.wallin@eyevinn.se>
2023-04-19 14:39:55 +0200

    fix: handle debouncing as a loading state in useSearch hook (#187)
    * fix: handle debouncing as a loading state in useSearch hook
    
    * fix: simplify isLoadingOrDebouncing assignment

--- packages/whitelabel-react/demo/components/SearchInput.tsx
+++ packages/whitelabel-react/demo/components/SearchInput.tsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 import { useSearch } from "../../src/index";
 
 export default function SearchInput() {
-  const [term, setTerm] = useState();
-  const [assets] = useSearch(term);
+  const [term, setTerm] = useState("");
+  const [assets, isLoading] = useSearch(term);
   return (
     <div>
       <input placeholder="Search" onChange={e => setTerm(e.target.value)} />
+      {isLoading && <p>Loading</p>}
       {assets?.map(a => (
         <Link key={a.assetId} to={`/asset/${a.assetId}`}>
           {a.title}
