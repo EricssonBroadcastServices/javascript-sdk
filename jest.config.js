24d15e19f473d15e4050523353bed0bb39f17e5b
Simon Wallin <simon.wallin@eyevinn.se>
2023-04-25 13:11:57 +0200

    test: add simple integration tests (#193)

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const tsconfig = require("./packages/exposure/tsconfig.json");
+
+module.exports = {
+  roots: ["<rootDir>/integration-tests"],
+  transform: {
+    "^.+\\.ts?$": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverage: true,
+  coverageReporters: ["text", "text-summary", "html"],
+  collectCoverageFrom: ["src/**/*.ts", "!**/node_modules/**"],
+  testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      tsconfig: {
+        ...tsconfig.compilerOptions,
+        esModuleInterop: true
+      }
+    }
+  }
+};
