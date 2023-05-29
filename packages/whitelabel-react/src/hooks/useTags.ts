dd52f4e1d2a09b2337f61c9a9ba3d511fa149467
Benjamin Wallberg <benjamin.wallberg@eyevinn.se>
2023-05-29 12:41:04 +0200

    feat(whitelabel-react): add/remove tag hooks (#211)

---
+++ packages/whitelabel-react/src/hooks/useTags.ts
@@ -0,0 +1,63 @@
+import { PreferenceListTags } from "@ericssonbroadcastservices/exposure-sdk";
+import { useQuery } from "react-query";
+import { queryClient, QueryKeys } from "../util/react-query";
+import { TApiHook } from "../types/type.apiHook";
+import { useExposureApi } from "./useApi";
+import { useUserSession } from "./useUserSession";
+import { useCallback, useState } from "react";
+
+const TAG_FEED_LIST_ID = "tagfeed";
+
+export function useTagList(): TApiHook<PreferenceListTags> {
+  const exposureApi = useExposureApi();
+  const [userSession] = useUserSession();
+  const { data, isLoading, error } = useQuery([QueryKeys.TAGS_LIST, userSession?.sessionToken], () => {
+    if (!userSession?.isLoggedIn()) return;
+    return exposureApi.preferences.getTagsFromList({
+      listId: TAG_FEED_LIST_ID
+    });
+  });
+  return [data || null, isLoading, error];
+}
+
+export function useAddTag(tagId: string): TApiHook<() => void, () => void> {
+  const exposureApi = useExposureApi();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<any>(null);
+
+  const add = useCallback(async () => {
+    setLoading(true);
+    await exposureApi.preferences
+      .addTagToList({ listId: TAG_FEED_LIST_ID, tagId })
+      .then(() => {
+        queryClient.invalidateQueries(QueryKeys.TAGS_LIST);
+      })
+      .catch(e => {
+        setError(e);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return [add, loading, error];
+}
+
+export function useRemoveTag(tagId: string): TApiHook<() => void, () => void> {
+  const exposureApi = useExposureApi();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<any>(null);
+
+  const remove = useCallback(async () => {
+    setLoading(true);
+    await exposureApi.preferences
+      .deleteTagFromList({ listId: TAG_FEED_LIST_ID, tagId })
+      .then(() => {
+        queryClient.invalidateQueries(QueryKeys.TAGS_LIST);
+      })
+      .catch(e => {
+        setError(e);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return [remove, loading, error];
+}
