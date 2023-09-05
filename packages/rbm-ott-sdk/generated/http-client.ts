7d9f4ec63305db6a131bc838026cee81c631dcbf
Albin Larsson <albin.larsson@eyevinn.se>
2023-09-05 14:45:26 +0200

    refactor(rbmott-sdk): refactor toQueryString to sanitizeParams

--- packages/rbm-ott-sdk/generated/http-client.ts
+++ packages/rbm-ott-sdk/generated/http-client.ts
@@ -27,12 +27,13 @@ export type requestArgs = {
   ctx?: ServiceContext;
 };
 
-function toQueryString(query: QueryParams = {}): string {
+// Remove undefined values and join array values to comma separated strings
+function sanitizeParams(query: QueryParams = {}): Record<string, string> {
   const sanitized = Object.entries(query)
     .filter(([, val]) => typeof val !== "undefined")
     .map(([key, val]) => [key, Array.isArray(val) ? val.join(",") : val]);
 
-  return String(new URLSearchParams(Object.fromEntries(sanitized)));
+  return Object.fromEntries(sanitized);
 }
 
 function defaultErrorFactory(response: Response) {
@@ -40,7 +41,7 @@ function defaultErrorFactory(response: Response) {
 }
 
 export async function request<T = any>({ method, url, headers = {}, query = {}, body, ctx }: requestArgs): Promise<T> {
-  const fullUrl = Object.keys(query).length ? `${url}/?${toQueryString(query)}` : url;
+  const fullUrl = Object.keys(query).length ? `${url}/?${new URLSearchParams(sanitizeParams(query))}` : url;
   if (!Object.keys(headers).some(key => key.toLowerCase() === "content-type")) {
     headers["content-type"] = "application/json";
   }
