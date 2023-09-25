73bccf79b802a6e5410622a3450f707d3798c167
Oscar Nord <oscar.nord@gmail.com>
2023-09-25 17:24:07 +0200

    feat(EMP-20569): add useProgramProgress and useGetTimeLeft hook (#227)
    * feat(EMP-20569): add useProgramProgress and useGetTimeLeft hook
    
    This makes it possible to gett the watched progress for VOD assets.
    
    For a live event this will instead represent the time that has progressed of program as well as how many minutes that are left.
    
    * fix: cleanup useProgramProgress

---
+++ packages/whitelabel-react/src/hooks/useProgramProgress.ts
@@ -0,0 +1,56 @@
+import { useEffect, useMemo, useState } from "react";
+import { useAsset } from "./useAsset";
+import { getEventProgress, getTimeLeft } from "../util/programProgress";
+import { useBookmarkPercentage } from "./useBookmarks";
+import { useTranslations } from "./useTranslations";
+
+type TimeLeftProps = {
+  percentageWatched?: number;
+  durationMs?: number;
+};
+
+export function useGetTimeLeft({ percentageWatched, durationMs }: TimeLeftProps): string {
+  const [translations] = useTranslations();
+  const [timeLeft, setTimeLeft] = useState("");
+
+  useEffect(() => {
+    const time = getTimeLeft(percentageWatched, durationMs);
+    let hours = "";
+    let minutes = "";
+
+    if (time.hours > 1) {
+      hours = `${time.hours} ${translations?.getText(["DATES", "HOURS"])}`;
+    } else if (time.hours) {
+      hours = `${time.hours} ${translations?.getText(["DATES", "HOUR"])}`;
+    }
+    if (time.minutes > 1) {
+      minutes = `${time.minutes} ${translations?.getText(["DATES", "MINUTES"])}`;
+    } else if (time.minutes) {
+      minutes = `${time.minutes} ${translations?.getText(["DATES", "MINUTE"])}`;
+    }
+    if (hours || minutes) {
+      setTimeLeft(`${hours ? `${hours}, ` : ""}${minutes}`);
+    }
+  }, [durationMs, percentageWatched, translations]);
+
+  return timeLeft;
+}
+
+export function useProgramProgress({ assetId, live }: { assetId?: string; live?: boolean }): {
+  duration: number;
+  progress: number;
+} {
+  const [asset] = useAsset(assetId);
+  const [assetProgress] = useBookmarkPercentage(assetId);
+  const [duration, progress] = useMemo(() => {
+    if (live && asset?.programs) {
+      const programProgress = getEventProgress(asset.programs, asset.assetId);
+      return [programProgress.duration, programProgress.progress];
+    } else if (asset && assetProgress) {
+      return [asset.duration, assetProgress];
+    }
+    return [0, 0];
+  }, [asset, assetProgress, assetId, live]);
+
+  return { duration, progress };
+}
