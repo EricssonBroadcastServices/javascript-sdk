2b192a4ad37de07e7571326bcc8f96043fff5080
simonwallin <simon.wallin@eyevinn.se>
2023-09-27 13:51:24 +0200

    feat: add translations

---
+++ packages/app-sdk/src/utils/wl-translations.spec.ts
@@ -0,0 +1,22 @@
+import { Translations } from "./wl-translations";
+
+const mockTranslations = new Translations({
+  ...englishTranslations,
+  deep: {
+    test: [{ test: "test" }]
+  }
+});
+
+describe("translations", () => {
+  it("should properly get text", () => {
+    expect(mockTranslations.getText("BUY")).toBe(mockTranslations.data["BUY"]);
+  });
+  it("should properly deepGet text", () => {
+    expect(mockTranslations.getText(["deep", "test", 0, "test"])).toBe("test");
+  });
+  it("should fallback to emprty string when called with undefined", () => {
+    /* eslint-disable */
+    // @ts-ignore
+    expect(mockTranslations.getText(undefined)).toBe("");
+  });
+});
