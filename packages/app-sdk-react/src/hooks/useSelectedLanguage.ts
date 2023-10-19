2f8e5afae47cb79cfa6cd2acff536ad2cb13dafe
Albin Larsson <albin.larsson@eyevinn.se>
2023-10-20 00:17:18 +0200

    refactor: migrate useSelectedLanguage

--- packages/app-sdk-react/src/hooks/useSelectedLanguage.ts
+++ packages/app-sdk-react/src/hooks/useSelectedLanguage.ts
@@ -1,6 +1,6 @@
-import { refetchUserDetails, useUserDetails } from "./useUserDetails";
 import { useCallback } from "react";
-import { useDeprecatedExposureApi } from "./useApi";
+import { updateUserDetails } from "@ericssonbroadcastservices/rbm-ott-sdk";
+import { refetchUserDetails, useUserDetails } from "./useUserDetails";
 import { useRedBeeStateDispatch, ActionType, useRedBeeState } from "../RedBeeProvider";
 import { useUserSession } from "./useUserSession";
 import { StorageKey } from "../util/storageKeys";
@@ -9,27 +9,24 @@ import { StorageKey } from "../util/storageKeys";
 const defaultLanguage = "en";
 
 export function useSetSelectedLanguage() {
-  const deprecatedExposureApi = useDeprecatedExposureApi();
   const dispatch = useRedBeeStateDispatch();
-  const [userSession] = useUserSession();
-  const { storage } = useRedBeeState();
+  const [session] = useUserSession();
+  const { storage, serviceContext } = useRedBeeState();
   const [userDetails] = useUserDetails();
   return useCallback(
-    (language: string, updateUserLanguage = true) => {
-      dispatch({ type: ActionType.SET_SELECTED_LANGUAGE, language: language });
+    async (language: string, updateUserLanguage = true) => {
+      dispatch({ type: ActionType.SET_SELECTED_LANGUAGE, language });
       if (storage) {
         storage.setItem(StorageKey.LOCALE, language);
       }
-      if (!userSession?.isLoggedIn() || language === userDetails?.language || !updateUserLanguage)
+      if (!session?.isLoggedIn() || language === userDetails?.language || !updateUserLanguage) {
         return Promise.resolve();
-      return (
-        deprecatedExposureApi.user
-          // TODO: this method should be changed in the api to only take relevant paramters
-          .updateUserDetails({ body: { language, displayName: null, newPassword: null } })
-          .then(() => refetchUserDetails())
-      );
+      }
+      const headers = { Authorization: `Bearer ${session?.sessionToken}` };
+      await updateUserDetails.call(serviceContext, { language, headers });
+      refetchUserDetails();
     },
-    [deprecatedExposureApi, dispatch]
+    [serviceContext, dispatch]
   );
 }
 
