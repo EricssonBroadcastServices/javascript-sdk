ce7fd2d0cca879bfbd7e38109a295227874472fa
Albin Larsson <albin.larsson@eyevinn.se>
2023-10-19 21:25:00 +0200

    refactor: migrate useAsset hook to new sdks

--- packages/app-sdk-react/src/hooks/useAsset.ts
+++ packages/app-sdk-react/src/hooks/useAsset.ts
@@ -1,23 +1,17 @@
-import { WLAsset } from "@ericssonbroadcastservices/whitelabel-sdk";
 import { useQuery } from "react-query";
+import { Asset } from "@ericssonbroadcastservices/rbm-ott-sdk";
+
 import { useRedBeeState } from "../RedBeeProvider";
-import { useDeprecatedWLApi } from "./useApi";
 import { TApiHook } from "../types/type.apiHook";
 import { QueryKeys } from "../util/react-query";
 
-export function useAsset(identifier?: string): TApiHook<WLAsset> {
-  const deprecatedWlApi = useDeprecatedWLApi();
-  const { customer, businessUnit, selectedLanguage } = useRedBeeState();
+export function useAsset(assetId?: string): TApiHook<Asset> {
+  const { customer, businessUnit, selectedLanguage, appService } = useRedBeeState();
   const { data, isLoading, error } = useQuery(
-    [QueryKeys.ASSET, identifier, customer, businessUnit, selectedLanguage],
+    [QueryKeys.ASSET, assetId, customer, businessUnit, selectedLanguage],
     () => {
-      if (!customer || !businessUnit || !identifier) return;
-      return deprecatedWlApi.getAssetById({
-        assetId: identifier,
-        customer,
-        businessUnit,
-        locale: selectedLanguage as string
-      });
+      if (!customer || !businessUnit || !assetId) return;
+      return appService.getAssetById(assetId);
     },
     { staleTime: 1000 * 60 * 10 }
   );
