0bd7bde404a858c32754047d0a4180abdcfb21f2
Albin Larsson <albin.larsson@eyevinn.se>
2023-10-24 15:03:15 +0200

    refactor: use PushNextContent instead of custom on our pnc hook

--- packages/app-sdk-react/src/hooks/usePushNextContentData.ts
+++ packages/app-sdk-react/src/hooks/usePushNextContentData.ts
@@ -2,17 +2,13 @@ import { useEffect, useState } from "react";
 import { Asset } from "@ericssonbroadcastservices/rbm-ott-sdk";
 import { useRedBeeState } from "../RedBeeProvider";
 import { TApiHook } from "../types/type.apiHook";
-
-interface pncState {
-  upNext?: Asset;
-  recommendations: Asset[];
-}
+import { PushNextContent } from "@ericssonbroadcastservices/app-sdk";
 
 export function usePushNextContentData(
   assetId?: string
 ): TApiHook<{ upNext?: Asset; recommendations: Asset[] | null }> {
   const { customer, businessUnit, appService } = useRedBeeState();
-  const [pushNextContent, setPushNextContent] = useState<pncState | null>(null);
+  const [pushNextContent, setPushNextContent] = useState<PushNextContent | null>(null);
   const [error, setError] = useState(null);
   useEffect(() => {
     if (assetId) {
