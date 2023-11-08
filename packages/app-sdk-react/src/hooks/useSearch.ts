45f12f325d22b1eb735166223b23406dcf1f9693
simonwallin <simon.wallin@eyevinn.se>
2023-11-08 10:23:38 +0100

    fix: use proper params in useSearch

--- packages/app-sdk-react/src/hooks/useSearch.ts
+++ packages/app-sdk-react/src/hooks/useSearch.ts
@@ -6,18 +6,21 @@ import { useDebounce } from "./useDebounce";
 import { SearchList, searchAsset } from "@ericssonbroadcastservices/rbm-ott-sdk";
 import { useSystemConfigV2 } from "./useSystemConfig";
 import { useCountryCode } from "./useGeolocation";
+import { useConfig } from "./useConfig";
 
 export function useSearch(term: string, debounceTime = 300): TApiHook<SearchList> {
   const ctx = useServiceContext();
   const debouncedTerm = useDebounce(term, debounceTime);
   const [systemConfig] = useSystemConfigV2();
   const countryCode = useCountryCode();
+  const [config] = useConfig();
   const { data, isLoading, error } = useQuery([QueryKeys.SEARCH, debouncedTerm], () => {
     if (!debouncedTerm || term === "") return;
     return searchAsset.call(ctx, {
       query: term,
-      locales: systemConfig?.frontendFeatures.searchLocales,
-      allowedCountry: countryCode
+      locale: systemConfig?.frontendFeatures.searchLocales,
+      allowedCountry: countryCode,
+      types: config?.parameters.assetSearchTypes
     });
   });
   const isLoadingOrDebouncing = term !== "" && (isLoading || term !== debouncedTerm);
