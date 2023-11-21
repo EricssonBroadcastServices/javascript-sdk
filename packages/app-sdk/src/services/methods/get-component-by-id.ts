c45f244419dad5df088a5266874183e7d6764ba2
simonwallin <simon.wallin@eyevinn.se>
2023-11-21 16:04:31 +0100

    fix: change values of deviceGroup to match cms definition

--- packages/app-sdk/src/services/methods/get-component-by-id.ts
+++ packages/app-sdk/src/services/methods/get-component-by-id.ts
@@ -1,7 +1,6 @@
 import { getWLComponent } from "@ericssonbroadcastservices/rbm-ott-sdk";
 import { IExposureComponent } from "../../interfaces/exposure-wl-component";
 import { WhiteLabelServiceContext } from "../white-label-service";
-import { DeviceGroup } from "../../interfaces/device-group";
 
 export interface GetComponentByIdOptions {
   componentId: string;
@@ -12,13 +11,12 @@ export async function getComponentById<T extends IExposureComponent>(
   context: WhiteLabelServiceContext,
   { componentId, countryCode }: GetComponentByIdOptions
 ): Promise<T> {
-  const platformDeviceDefinition = Object.keys(DeviceGroup).find(v => DeviceGroup[v] === context.deviceGroup);
   return (
     await getWLComponent.call(context, {
       configId: "sandwich",
       allowedCountry: countryCode,
       componentId,
-      filters: `DEVICE:${platformDeviceDefinition}`
+      filters: `DEVICE:${context.deviceGroup}`
     })
   ).json();
 }
--- packages/app-sdk/src/services/methods/get-config-by-customer-and-businessUnit.ts
+++ packages/app-sdk/src/services/methods/get-config-by-customer-and-businessUnit.ts
@@ -1,7 +1,6 @@
 import { getWLConfig } from "@ericssonbroadcastservices/rbm-ott-sdk";
 import { WhiteLabelServiceContext } from "../white-label-service";
 import { IExposureWLConfig } from "../../interfaces/exposure-wl-config";
-import { DeviceGroup } from "../../interfaces/device-group";
 
 export interface GetConfigByCustomerAndBusinessUnitOptions {
   countryCode: string;
@@ -11,12 +10,11 @@ export async function getConfigByCustomerAndBusinessUnit(
   context: WhiteLabelServiceContext,
   { countryCode }: GetConfigByCustomerAndBusinessUnitOptions
 ) {
-  const platformDeviceDefinition = Object.keys(DeviceGroup).find(v => DeviceGroup[v] === context.deviceGroup);
   return (
     await getWLConfig.call(context, {
       configId: "sandwich",
       allowedCountry: countryCode,
-      filters: `DEVICE:${platformDeviceDefinition}`
+      filters: `DEVICE:${context.deviceGroup}`
     })
   ).json() as Promise<IExposureWLConfig>;
 }
