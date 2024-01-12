e8c3007dfcbc24b56325a35fa96803156fbd8911
Benjamin Wallberg <benjamin.wallberg@eyevinn.se>
2024-01-12 16:04:04 +0100

    fix: percentage hook returned not percentage

--- packages/app-sdk-react/src/hooks/useAssetDisplay.ts
+++ packages/app-sdk-react/src/hooks/useAssetDisplay.ts
@@ -35,7 +35,7 @@ export function useAssetDisplayTvShow(asset: Asset, options: UseAssetOptions) {
   const [continueWatching, loadingContinueWatching] = useContinueWatching(asset.assetId);
   const [entitlement, loadingEntitlement] = useEntitlementForAsset({ asset: continueWatching?.asset }, {});
 
-  const bookmarkPercentage = useBookmarkPercentage(continueWatching?.asset?.assetId);
+  const [bookmarkPercentage] = useBookmarkPercentage(continueWatching?.asset?.assetId);
 
   const seasons = useMemo(() => {
     if (asset.seasons) {
