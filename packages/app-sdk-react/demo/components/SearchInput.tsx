f588f4227f3f9817fd6ac8b63e48e26ed01102a4
simonwallin <simon.wallin@eyevinn.se>
2024-01-17 16:46:56 +0100

    feat: add expanded search hook

--- packages/app-sdk-react/demo/components/SearchInput.tsx
+++ packages/app-sdk-react/demo/components/SearchInput.tsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useSearch, useSelectedLanguage } from "../../src/index";
+import { useSelectedLanguage } from "../../src/index";
 import { AssetHelpers } from "@ericssonbroadcastservices/app-sdk";
+import { useExpandedSearch } from "../../src/hooks/useSearch";
 
 export default function SearchInput() {
   const [term, setTerm] = useState("");
-  const [result, isLoading] = useSearch(term);
+  const [result, isLoading] = useExpandedSearch({ term, schemes: [] });
   const locale = useSelectedLanguage();
   return (
     <div style={{ marginLeft: "10px" }}>
       <input placeholder="Search" onChange={e => setTerm(e.target.value)} />
       {isLoading && <p>Loading</p>}
-      {result?.items?.map(a => (
-        <Link key={a.asset.assetId} to={`/asset/${a.asset.assetId}`}>
-          {AssetHelpers.getTitle(a.asset, locale)}
+      {result?.map(a => (
+        <Link key={a.assetId} to={`/asset/${a.assetId}`}>
+          {AssetHelpers.getTitle(a, locale)}
         </Link>
       ))}
     </div>
