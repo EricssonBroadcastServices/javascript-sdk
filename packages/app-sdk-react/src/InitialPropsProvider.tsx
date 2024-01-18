bdbc68f1d5ca1e82ffeac122c05016d6cd4f44d7
Simon Wallin <simon.wallin@eyevinn.se>
2024-01-18 13:54:10 +0100

    feat: init session from sessionToken (#275)

--- packages/app-sdk-react/src/InitialPropsProvider.tsx
+++ packages/app-sdk-react/src/InitialPropsProvider.tsx
@@ -1,17 +1,12 @@
-import {
-  DeviceRegistration,
-  ServiceContext,
-  loginAnonymous,
-  validateSessionToken
-} from "@ericssonbroadcastservices/rbm-ott-sdk";
+import { DeviceRegistration, ServiceContext } from "@ericssonbroadcastservices/rbm-ott-sdk";
 import { WhiteLabelService as AppService, DeviceGroup } from "@ericssonbroadcastservices/app-sdk";
 
 import React, { useEffect, useState } from "react";
 import { IStorage } from ".";
 import { IRedBeeState } from "./RedBeeProvider";
-import { ErrorCode } from "./util/error";
 import { StorageKey } from "./util/storageKeys";
 import { Session, SessionData } from "./Session";
+import { getValidatedPersistedSession, validateAndReconstructSessionFromSessionToken } from "./util/session-token";
 
 export const InitialPropsContext = React.createContext<IRedBeeState>({} as IRedBeeState);
 
@@ -24,58 +19,7 @@ interface IInitialPropsProvider {
   children?: React.ReactNode;
   deviceGroup: DeviceGroup;
   onSessionValidationError?: (err: unknown) => void;
-}
-
-async function getValidatedPersistedSession({
-  storage,
-  customer,
-  businessUnit,
-  baseUrl,
-  deviceRegistration
-}: {
-  customer: string;
-  businessUnit: string;
-  storage?: IStorage;
-  baseUrl: string;
-  deviceRegistration: Required<DeviceRegistration>;
-}): Promise<[SessionData | null, unknown]> {
-  const ctx = { customer, businessUnit, baseUrl };
-  let session: SessionData | null = null;
-  let error: unknown = null;
-  const persistedSession = await storage?.getItem(StorageKey.SESSION);
-  if (persistedSession) {
-    const persistedSessionJSON = JSON.parse(persistedSession);
-    if (!persistedSessionJSON.sessionToken) {
-      storage?.removeItem(StorageKey.SESSION);
-      session = null;
-    } else {
-      session = persistedSessionJSON;
-      try {
-        // this will throw if session is invalid
-        const headers = { Authorization: `Bearer ${persistedSessionJSON.sessionToken}` };
-        await validateSessionToken.call(ctx, { headers });
-      } catch (err) {
-        if ((err as any).response?.status === 401) {
-          storage?.removeItem(StorageKey.SESSION);
-          session = null;
-        } else {
-          error = { code: ErrorCode.UNEXPECTED_SESSION_VALIDATION_ERROR, error: err, session };
-        }
-      }
-    }
-  }
-  if (!session) {
-    try {
-      const { deviceId, ...device } = deviceRegistration;
-      session = new Session({
-        ...(await loginAnonymous.call(ctx, { device, deviceId })),
-        isAnonymous: true
-      });
-    } catch (err) {
-      throw err;
-    }
-  }
-  return [session, error];
+  sessionToken?: string;
 }
 
 export function InitialPropsProvider({
@@ -86,34 +30,50 @@ export function InitialPropsProvider({
   baseUrl,
   deviceRegistration,
   deviceGroup,
-  onSessionValidationError
+  onSessionValidationError,
+  sessionToken
 }: IInitialPropsProvider) {
   const [state, setState] = useState<IRedBeeState | null>(null);
   const [isReady, setIsReady] = useState(false);
   useEffect(() => {
     async function initStorage() {
-      let session: SessionData | null = null;
-      try {
-        const [validatedSession, validationError] = await getValidatedPersistedSession({
-          storage,
-          customer,
-          businessUnit,
-          baseUrl,
-          deviceRegistration
-        });
-        session = validatedSession;
-        if (validationError) {
-          onSessionValidationError?.(validationError);
-        }
-      } catch (err) {
-        session = null;
-      }
-      const persistedSelectedLanguage = await storage?.getItem(StorageKey.LOCALE);
       const serviceContext: ServiceContext = {
         customer,
         businessUnit,
         baseUrl
       };
+
+      let session: SessionData | null = null;
+
+      // if a sessionToken has been passed in: use that if possible
+      if (sessionToken) {
+        session = await validateAndReconstructSessionFromSessionToken({
+          context: serviceContext,
+          sessionToken,
+          storage
+        });
+      }
+      // if there still is no session, use one from storage, if possible
+      if (!session) {
+        try {
+          const [validatedSession, validationError] = await getValidatedPersistedSession({
+            storage,
+            customer,
+            businessUnit,
+            baseUrl,
+            deviceRegistration
+          });
+          session = validatedSession;
+          if (validationError) {
+            onSessionValidationError?.(validationError);
+          }
+        } catch (err) {
+          session = null;
+        }
+      }
+
+      const persistedSelectedLanguage = await storage?.getItem(StorageKey.LOCALE);
+
       async function getAuthToken() {
         return session?.sessionToken;
       }
@@ -136,6 +96,7 @@ export function InitialPropsProvider({
       setIsReady(true);
     }
     initStorage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   if (!isReady || !state) return null;
   return <InitialPropsContext.Provider value={state}>{children}</InitialPropsContext.Provider>;
