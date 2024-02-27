ee27a9da2b5cde382cbb6bcb1d97d904518ca75e
Arkadiusz Polak <158449705+arkadiusz-polak@users.noreply.github.com>
2024-02-27 10:12:21 +0100

    feat: do not require password for account deletion (#305)
    * feat: updates spec for account delete, useDeleteAccount hook update
    
    * fix: removed spec path to deprecated method to user deleting
    
    ---------
    
    Co-authored-by: Arkadiusz Polak <apolak@tvcoins.com>

--- packages/app-sdk-react/demo/components/DeleteAccountButton.tsx
+++ packages/app-sdk-react/demo/components/DeleteAccountButton.tsx
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDeleteAccount, useUserSession } from "../../src";
 
 export const DeleteAccountButton = () => {
   const [session] = useUserSession();
   const [deleteAccount] = useDeleteAccount();
-  const [password, setPassword] = useState("");
   if (!session?.isLoggedIn()) {
     return null;
   }
   return (
     <div>
-      <input type="text" placeholder="password" onChange={e => setPassword(e.target.value)} />
-      <button onClick={() => deleteAccount(password)}>Delete account test</button>
+      <button onClick={() => deleteAccount()}>Delete account test</button>
     </div>
   );
 };
--- packages/app-sdk-react/src/hooks/useDeleteAccount.ts
+++ packages/app-sdk-react/src/hooks/useDeleteAccount.ts
@@ -1,11 +1,11 @@
-import { deleteUserDetails } from "@ericssonbroadcastservices/rbm-ott-sdk";
+import { deleteDetails } from "@ericssonbroadcastservices/rbm-ott-sdk";
 import { useServiceContext } from "./useApi";
 import { useSetSession, useUserSession } from "./useUserSession";
 import { TApiMutation } from "../types/type.apiHook";
 import { useMutation } from "react-query";
 import { useAppError } from "./useAppError";
 
-export function useDeleteAccount(): TApiMutation<string, void> {
+export function useDeleteAccount(): TApiMutation<void, void> {
   const [session] = useUserSession();
   const setSession = useSetSession();
   const ctx = useServiceContext();
@@ -15,12 +15,12 @@ export function useDeleteAccount(): TApiMutation<string, void> {
       setSession(null);
     },
     mutationKey: [session, ctx, setSession],
-    mutationFn: async (password: string) => {
+    mutationFn: async () => {
       if (!session?.isLoggedIn()) {
         throw new Error("User needs to be logged in to request account deletion");
       }
       const headers = { Authorization: `Bearer ${session.sessionToken}` };
-      await deleteUserDetails.call(ctx, { password, headers });
+      await deleteDetails.call(ctx, { headers });
       setSession(null);
     }
   });
